c933740a432426f4fd1350890ed64f388279d89e
invincible <invincibleresearcher@gmail.com>
2020-10-16 22:17:31 +0900

    simple queue & stack

---
+++ queue.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define MAX_LEN 100
+
+typedef struct {
+    int buf[MAX_LEN];
+    int wptr, rptr;
+    int size;
+} queue;
+
+void queue_init(queue *q)
+{
+    q->size = q->wptr = q->rptr = 0;
+}
+
+void enq(queue *q, int d)
+{
+    q->buf[q->wptr] = d;
+    q->wptr++;
+    q->size++;
+    if (q->wptr >= MAX_LEN) q->wptr -= MAX_LEN;
+}
+
+int deq(queue *q)
+{
+    int d = q->buf[q->rptr];
+    q->rptr++;
+    q->size--;
+    if (q->rptr >= MAX_LEN) q->rptr -= MAX_LEN;
+    return d;
+}
+
+int queue_size(queue *q)
+{
+    return q->size;
+}
+
+int main(void)
+{
+    queue s;
+
+    queue_init(&s);
+    enq(&s, 1);
+    enq(&s, 2);
+    enq(&s, 3);
+    enq(&s, 4);
+    printf("size:%d\n", queue_size(&s));
+
+    printf("%d\n", deq(&s));
+    printf("%d\n", deq(&s));
+    printf("%d\n", deq(&s));
+
+    enq(&s, 10);
+    enq(&s, 20);
+
+    printf("%d\n", deq(&s));
+    printf("%d\n", deq(&s));
+    printf("%d\n", deq(&s));
+
+    printf("size:%d\n", queue_size(&s));
+    
+    return 0;
+}
---
+++ stack.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define MAX_LEN 100
+
+typedef struct {
+    int buf[MAX_LEN];
+    int wptr;
+} stack;
+
+void stack_init(stack *s)
+{
+    s->wptr = 0;
+}
+
+void push(stack *s, int d)
+{
+    s->buf[s->wptr] = d;
+    s->wptr++;
+}
+
+int pop(stack *s)
+{
+    int v = s->buf[s->wptr-1];
+    s->wptr--;
+    return v;
+}
+
+int stack_size(stack *s)
+{
+    return s->wptr;
+}
+
+int main(void)
+{
+    stack s;
+
+    stack_init(&s);
+    push(&s, 1);
+    push(&s, 2);
+    push(&s, 3);
+    push(&s, 4);
+    printf("size:%d\n", stack_size(&s));
+
+    printf("%d\n", pop(&s));
+    printf("%d\n", pop(&s));
+    printf("%d\n", pop(&s));
+
+    push(&s, 10);
+    push(&s, 20);
+
+    printf("%d\n", pop(&s));
+    printf("%d\n", pop(&s));
+    printf("%d\n", pop(&s));
+
+    printf("size:%d\n", stack_size(&s));
+    return 1;
+}
