95c143d3598d907b0f69776975fe43b6a9c58da3
invincibleresearcher <34017201+invincibleresearcher@users.noreply.github.com>
2021-04-01 19:31:42 +0900

    Update segment_tree1.c

--- segment_tree1.c
+++ segment_tree1.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+// the below headers are just fo tcs
+#include <stdint.h>
+#include <stdlib.h>
 
 #define MAX_N 10000
 
@@ -6,6 +9,11 @@
 #define R_CHILD(pi) (2*pi+1)
 #define PARENT(ci) (ci/2)
 
+enum opt {
+	OPT_FIND_SUM,
+	OPT_FIND_MAX,
+	OPT_FIND_MIN,
+};
 // https://www.geeksforgeeks.org/segment-tree-efficient-implementation/
 
 struct sg_tree {
@@ -13,9 +21,19 @@ struct sg_tree {
 	int size;
 };
 
+enum opt option = OPT_FIND_SUM;
+
 static inline int update_rule(int l_child, int r_child)
 {
-	return l_child + r_child;
+	if (option == OPT_FIND_SUM) {
+		return l_child + r_child;
+	}
+	else if (option == OPT_FIND_MAX) {
+		return l_child > r_child ? l_child : r_child;
+	}
+	else {
+		return l_child > r_child ? r_child : l_child;
+	}
 }
 
 static inline void sg_update(struct sg_tree *t, int pi)
@@ -52,11 +70,20 @@ static inline int query(struct sg_tree *t, int l, int r)
 {
 	int n = t->size, res = 0;
 
-	for (l += n, r += n; l < r; l >>= 1, r >>= 1)
+	if (option == OPT_FIND_SUM) {
+		res = 0;
+	}
+	else if (option == OPT_FIND_MAX) {
+		res = INT32_MIN;
+	}
+	else {
+		res = INT32_MAX;
+	}
+
+	for (l += n, r += n; l < r; l /= 2, r /= 2)
 	{
-		printf("\t\tl: %d r: %d res: %d\n", l, r, res);
-		if (l&1) res += t->data[l++];
-		if (r&1) res += t->data[--r];
+		if (l&1) res = update_rule(res, t->data[l++]);
+		if (r&1) res = update_rule(res, t->data[--r]);
 	}
 	return res;
 }
@@ -64,21 +91,50 @@ static inline int query(struct sg_tree *t, int l, int r)
 static inline int real_query(struct sg_tree *t, int l, int r)
 {
 	int i, res = 0;
-	for (i = l; i < r; i++) {
-		res = update_rule(res, t->data[t->size + i]);
+	if (option == OPT_FIND_SUM) {
+		for (i = l; i < r; i++) {
+			res += t->data[t->size + i];
+		}
+	}
+	else if (option == OPT_FIND_MAX) {
+		res = t->data[t->size + l];
+		for (i = l+1; i < r; i++) {
+			if (t->data[t->size + i] > res) {
+				res = t->data[t->size + i];
+			}
+		}
+	}
+	else {
+		res = t->data[t->size + l];
+		for (i = l+1; i < r; i++) {
+			if (t->data[t->size + i] < res) {
+				res = t->data[t->size + i];
+			}
+		}
 	}
 	return res;
 }
 
-#define QUERY_TEST(t, a,b) do {\
-	printf("## [%d, %d) - query: %d - real answer: %d\n", a, b, query(t, a, b), real_query(t, a, b)); \
+#define QUERY_TEST(t, a, b) do {\
+	int r1, r2; \
+	r1 = query(t, a, b); \
+	r2 = real_query(t, a, b); \
+	if (r1 == r2) { \
+		printf("## PASSED [%d, %d) res: %d\n", a, b, r1); \
+	} \
+	else { \
+		printf("!! WRONG !! [%d, %d) - query: %d - real answer: %d\n", a, b, r1, r2); \
+		exit(-1); \
+	} \
 } while(0)
 
-int main(void) {
+static void test1(void)
+{
 	struct sg_tree t;
 	int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-
 	build_sg(&t, a, sizeof(a)/sizeof(a[0]));
+
+	printf("## %s start\n", __func__);
 
 	QUERY_TEST(&t, 0, 12);
 	QUERY_TEST(&t, 0, 3);
@@ -89,6 +145,76 @@ int main(void) {
 	QUERY_TEST(&t, 3, 8);
 	QUERY_TEST(&t, 2, 7);
 	QUERY_TEST(&t, 4, 10);
+}
 
+static void test2(void)
+{
+	struct sg_tree t;
+	int a[] = {1, 2, 3, 4, 5};
+	build_sg(&t, a, sizeof(a)/sizeof(a[0]));
+
+	printf("## %s start\n", __func__);
+
+	QUERY_TEST(&t, 0, 2);
+	QUERY_TEST(&t, 2, 4);
+	QUERY_TEST(&t, 0, 4);
+	QUERY_TEST(&t, 0, 5);
+
+	QUERY_TEST(&t, 3, 4);
+	QUERY_TEST(&t, 3, 5);
+	QUERY_TEST(&t, 1, 4);
+}
+
+static void test3(void)
+{
+	struct sg_tree t;
+	int a[] = {-1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12};
+	build_sg(&t, a, sizeof(a)/sizeof(a[0]));
+
+	printf("## %s start\n", __func__);
+
+	QUERY_TEST(&t, 0, 12);
+	update_node(&t, 0, 2);
+	QUERY_TEST(&t, 0, 12);
+
+	QUERY_TEST(&t, 0, 3);
+	update_node(&t, 1, 12);
+	QUERY_TEST(&t, 0, 3);
+
+	QUERY_TEST(&t, 0, 2);
+	QUERY_TEST(&t, 0, 1);
+
+	QUERY_TEST(&t, 2, 12);
+	update_node(&t, 5, 88);
+	QUERY_TEST(&t, 2, 12);
+	QUERY_TEST(&t, 3, 8);
+	update_node(&t, 5, 21);
+	QUERY_TEST(&t, 3, 8);
+	update_node(&t, 5, 1);
+	QUERY_TEST(&t, 3, 8);
+	QUERY_TEST(&t, 2, 7);
+	QUERY_TEST(&t, 4, 10);
+}
+
+
+int main(void) {
+
+	option = OPT_FIND_SUM;
+	printf("## find sum start..\n");
+	test1();
+	test2();
+	test3();
+
+	option = OPT_FIND_MAX;
+	printf("## find max start..\n");
+	test1();
+	test2();
+	test3();
+
+	option = OPT_FIND_MIN;
+	printf("## find min start..\n");
+	test1();
+	test2();
+	test3();
 	return 0;
 }
