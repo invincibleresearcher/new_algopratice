bca3f6b9d8d14f0903b5ba2e66d9ba7409df5e26
invincibleresearcher <34017201+invincibleresearcher@users.noreply.github.com>
2021-04-02 19:21:21 +0900

    Create rect_overlap_check.c

---
+++ rect_overlap_check.c
@@ -0,0 +1,340 @@
+#include <stdio.h>
+// the below are just for tcs & debug
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+static inline long timer_check(void)
+{
+	static struct timespec prev_ts;
+	struct timespec ts;
+	long tdiff = 0;
+	int ret = clock_gettime(CLOCK_MONOTONIC, &ts);
+	tdiff = (ts.tv_sec - prev_ts.tv_sec) * 1000 +
+		(ts.tv_nsec - prev_ts.tv_nsec) / 1000000;
+	memcpy(&prev_ts, &ts, sizeof(ts));
+	return tdiff;
+}
+
+#define MAX_C 10000
+#define MAX_R 10000
+
+#define GRID_R 2000
+#define GRID_C 2000
+
+#define MAX_RECT 10000
+
+#define DEFINE_STATIC_ALLOC(n, s) \
+struct n##_mem { \
+	struct n pool[s]; \
+	void *ptr[s]; \
+	int wptr; \
+}; \
+\
+struct n##_mem n##_mem_impl; \
+\
+static inline void n##_mem_init(void) \
+{ \
+	int i; \
+	for (i = 0; i < s; i++) n##_mem_impl.ptr[i] = &n##_mem_impl.pool[i]; \
+	n##_mem_impl.wptr = s; \
+} \
+\
+static inline void *n##_alloc(void) \
+{ \
+	return n##_mem_impl.ptr[--n##_mem_impl.wptr]; \
+} \
+\
+static inline void n##_free(void *ptr) \
+{ \
+	n##_mem_impl.ptr[n##_mem_impl.wptr++] = ptr; \
+}
+
+#define L_MAX_R_SIZE	(MAX_R/GRID_R)
+#define L_MAX_C_SIZE	(MAX_C/GRID_C)
+#define MAX_HEAD_SIZE	((L_MAX_R_SIZE*L_MAX_C_SIZE)+1)
+#define MAX_LN_SIZE	(MAX_HEAD_SIZE * MAX_RECT)
+
+enum overlap_op {
+	OVERLAP_DEFAULT,
+	OVERLAP_BOUNDARY_ONLY,
+};
+
+enum overlap_op option = OVERLAP_DEFAULT;
+
+#define INDEX(x,y,w) (y*w+x)
+
+struct rect {
+	int x, y, w, h;
+	char is_valid;
+	void *lnptr[MAX_HEAD_SIZE];
+};
+
+DEFINE_STATIC_ALLOC(rect, MAX_RECT)
+
+struct rect_ln {
+	struct rect *r;
+	struct rect_ln *prev, *next;
+};
+
+DEFINE_STATIC_ALLOC(rect_ln, MAX_LN_SIZE)
+
+struct rect_ln head[MAX_HEAD_SIZE];
+
+static inline void list_init(void)
+{
+	int i;
+	for (i = 0; i < MAX_HEAD_SIZE; i++) head[i].next = NULL;
+}
+
+static inline void __add_list(struct rect_ln *h, struct rect_ln *n)
+{
+	n->prev = h;
+	n->next = h->next;
+	if (h->next) h->next->prev = n;
+	h->next = n;
+}
+
+static inline void add_list(int i, struct rect *r)
+{
+	struct rect_ln *ln;
+
+	ln = (struct rect_ln *) rect_ln_alloc();
+	ln->r = r;
+	r->lnptr[i] = ln;
+	__add_list(&head[i], ln);
+}
+
+static inline struct rect *new_rect(int x, int y, int w, int h)
+{
+	int i;
+	struct rect *r = (struct rect *) rect_alloc();
+	r->is_valid = 1;
+	r->x = x; r->y = y; r->w = w; r->h = h;
+	for (i = 0; i < MAX_HEAD_SIZE; i++) r->lnptr[i] = NULL;
+	return r;
+}
+
+static inline int add_rect(int x, int y, int w, int h)
+{
+	int i, j, box_sx, box_sy, box_ex, box_ey;
+	struct rect *r;
+
+	if (x < 0 || y < 0) return -1;
+	if (x+w >= MAX_R || y+h > MAX_C) return -1;
+
+	r = new_rect(x, y, w, h);
+	add_list(0, r);
+
+	box_sx = x / GRID_C; box_ex = (x + w) / GRID_C;
+	box_sy = y / GRID_R; box_ey = (y + h) / GRID_R;
+
+	switch (option) {
+	case OVERLAP_DEFAULT:
+		for (i = box_sy; i <= box_ey; i++) {
+			for (j = box_sx; j <= box_ex; j++) {
+				add_list(INDEX(j, i, L_MAX_C_SIZE)+1, r);
+			}
+		}
+		break;
+	case OVERLAP_BOUNDARY_ONLY:
+		// FIXME: TDB.
+		break;
+	default:
+		break;
+	}
+	return 0;
+}
+
+static inline void __remove_list(struct rect_ln *n)
+{
+	n->prev->next = n->next;
+	if (n->next) n->next->prev = n->prev;
+	rect_ln_free(n);
+}
+
+static inline void remove_rect(struct rect *r)
+{
+	int i;
+	for (i = 0; i < MAX_HEAD_SIZE; i++) {
+		if (r->lnptr[i]) {
+			__remove_list((struct rect_ln *)r->lnptr[i]);
+		}
+	}
+	r->is_valid = 0;
+	rect_free(r);
+}
+
+static inline int __is_overlap_op1(int x1, int y1, int w1, int h1,
+				   int x2, int y2, int w2, int h2)
+{
+	if (x1+w1 < x2 || x2+w2 < x1) return 0;
+	if (y1+h1 < y2 || y2+y2 < y1) return 0;
+	return 1;
+}
+
+static inline int is_inside(int x1, int y1, int w1, int h1,
+			    int x2, int y2, int w2, int h2)
+{
+	if (x1 <= x2 && x2+w2 <= x1+w1 &&
+	    y1 <= y2 && y2+h2 <= y1+h1) {
+		return 1;
+	}
+	return 0;
+}
+
+static inline int __is_overlap_op2(int x1, int y1, int w1, int h1,
+				   int x2, int y2, int w2, int h2)
+{
+	if (x1+w1 <= x2 || x2+w2 <= x1) return 0;
+	if (y1+h1 <= y2 || y2+y2 <= y1) return 0;
+	if (is_inside(x1, y1, w1, h1, x2, y2, w2, h2)) return 0;
+	if (is_inside(x2, y2, w2, h2, x1, y1, w1, h1)) return 0;
+	return 1;
+}
+
+static inline int is_overlap(int x1, int y1, int w1, int h1,
+			     int x2, int y2, int w2, int h2)
+{
+	if (option == OVERLAP_DEFAULT) {
+		return __is_overlap_op1(x1, y1, w1, h1, x2, y2, w2, h2);
+	}
+	else {
+		return __is_overlap_op2(x1, y1, w1, h1, x2, y2, w2, h2);
+	}
+}
+
+static inline int __query_rect(struct rect_ln *head, int op, int x, int y, int w, int h)
+{
+	int cnt = 0;
+	struct rect *r;
+	struct rect_ln *it = head;
+
+	while (it->next) {
+		r = it->next->r;
+		cnt += is_overlap(r->x, r->y, r->w, r->h, x, y, w, h);
+		if (op == 0 && cnt) return 1;
+		it = it->next;
+	}
+	return cnt;
+}
+
+static inline int query_rect(int op, int x, int y, int w, int h)
+{
+	int cnt = 0;
+	int idx, i, j, box_sx, box_sy, box_ex, box_ey;
+
+	box_sx = x / GRID_C; box_ex = (x + w) / GRID_C;
+	box_sy = y / GRID_R; box_ey = (y + h) / GRID_R;
+	for (i = box_sy; i <= box_ey; i++) {
+		for (j = box_sx; j <= box_ex; j++) {
+			idx = INDEX(j, i, L_MAX_C_SIZE) + 1;
+			cnt += __query_rect(&head[idx], op, x, y, w, h);
+			if (cnt) return 1;
+		}
+	}
+	return cnt;
+}
+
+static inline int query_rect_ref(int op, int x, int y, int w, int h)
+{
+	int cnt = 0;
+	cnt += __query_rect(&head[0], op, x, y, w, h);
+	return cnt;
+}
+
+#define ADD_TEST(x,y,w,h) do {\
+	int ret = add_rect(x, y, w, h);\
+	if (ret) printf("@@@ [%d,%d,%d,%d] add fail\n", x, y, w, h); \
+} while(0)
+
+#define QUERY_TEST(x,y,w,h) do {\
+	int ret1, ret2; \
+	ret1 = query_rect(x,y,w,h); \
+	ret2 = query_rect_ref(x,y,w,h); \
+	if (ret1 != ret2) printf("@@@@ [%d,%d,%d,%d] query fail ret: %d ans: %d\n",\
+				 x, y, w, h, ret1, ret2);\
+} while(0)
+
+static void test1(void)
+{
+	int i, tmp, cnt = 0;
+	struct rect r;
+#define TEST1_QUERY_SIZE 3000
+	struct rect qr[TEST1_QUERY_SIZE];
+	int ret[TEST1_QUERY_SIZE], ans[TEST1_QUERY_SIZE];
+
+	printf("## %s start\n", __func__);
+	rect_mem_init();
+	rect_ln_mem_init();
+	list_init();
+
+	srand((unsigned int)time(NULL));
+
+	while (cnt < 6000) {
+		r.x = rand() % MAX_C;
+		if (r.x == MAX_C -1) r.x = MAX_C - 300;
+		tmp = rand() % GRID_C;
+		if (!tmp) tmp = GRID_C / 4;
+		r.w = tmp;
+		if (r.x + r.w >= MAX_C) r.w = MAX_C -1 - r.x;
+
+		r.y = rand() % MAX_R;
+		if (r.y == MAX_R -1) r.y = MAX_R - 300;
+		tmp = rand() % GRID_R;
+		if (!tmp) tmp = GRID_R / 4;
+		r.h = tmp;
+		if (r.y + r.h >= MAX_R) r.h = MAX_R -1 - r.y;
+
+		ADD_TEST(r.x, r.y, r.w, r.h);
+		cnt++;
+	}
+
+	cnt = 0;
+	while (cnt < TEST1_QUERY_SIZE) {
+		tmp = rand() % MAX_C;
+		if (!tmp) tmp = MAX_C / 4;
+		qr[cnt].x = rand() % tmp;
+		qr[cnt].w = tmp - qr[cnt].x;
+		tmp = rand() % MAX_R;
+		if (!tmp) tmp = MAX_C / 4;
+		qr[cnt].y = rand() % tmp;
+		qr[cnt].h = tmp - qr[cnt].y;
+		cnt++;
+	}
+
+	timer_check();
+	cnt = 0;
+	while (cnt < TEST1_QUERY_SIZE) {
+		ans[cnt] = query_rect_ref(0, qr[cnt].x, qr[cnt].y, qr[cnt].w, qr[cnt].h);
+		cnt++;
+	}
+	printf("cal time: %ld ms\n", timer_check());
+
+	timer_check();
+	cnt = 0;
+	while (cnt < TEST1_QUERY_SIZE) {
+		ret[cnt] = query_rect(0, qr[cnt].x, qr[cnt].y, qr[cnt].w, qr[cnt].h);
+		cnt++;
+	}
+	printf("cal time: %ld ms\n", timer_check());
+
+	cnt = 0;
+	while (cnt < TEST1_QUERY_SIZE) {
+		if (ans[cnt] != ret[cnt]) {
+			printf("[%d test] ans:%d != ret:%d [%d,%d,%d,%d]\n",
+			       cnt, ans[cnt], ret[cnt],
+			       qr[cnt].x, qr[cnt].y, qr[cnt].w, qr[cnt].h);
+			exit(-1);
+		}
+		cnt++;
+	}
+
+	printf("## %s end\n", __func__);
+}
+
+int main(void)
+{
+	test1();
+	return 0;
+}
